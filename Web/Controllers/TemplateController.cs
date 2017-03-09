73c6f6a7d00aec1ee80e30284e291c37d28f7d0c
Augustin Nshimiyiman <Augustin Nshimiyiman@DESKTOP-KSDU36P>
2017-03-08 19:22:23 -0600

    Updated Structure

--- Web/Controllers/TemplateController.cs
+++ Web/Controllers/TemplateController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AspNet.Identity.Dapper;
-using PPOK_System.Domain;
+using ppok_refill.Models;
 
 namespace Web.Controllers
 {
---
+++ ppok_refill.Models/CommunicationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppok_refill.Models
+{
+    public class CommunicationType
+    {
+        public int ID { get; set; }
+        public string comm_type_name { get; set; }
+        public string comm_type_description { get; set; }
+    }
+}
